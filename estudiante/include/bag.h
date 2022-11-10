ee83d8e5dbcc9c20bcc8375ae3616c0899080cf9
diegoos03 <diorsa03@gmail.com>
2022-11-10 21:37:27 +0100

    actualizado

--- estudiante/include/bag.h
+++ estudiante/include/bag.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,9 +24,52 @@ private:
 public:
     Bag() = default;
 
-    Bag(const Bag<T> &other) {};
+    Bag(const Bag<T> &other);
 
+    void add(const T & element);
+
+    T get();
+
+    void clear();
+
+    unsigned int size() const;
+
+    bool empty();
+
+    const Bag<T>& operator= (const Bag<T> & other);
 
 };
+
+template<class T> Bag<T>::Bag(const Bag<T> &other) {
+    v = other.v;
+};
+
+template<class T>void Bag<T>::add (const T & element) {
+    v.push_back(element);
+};
+
+template<class T>T Bag<T>::get(){
+    int random = rand() % v.size();
+    T element = v[random];
+    v.erase(v.begin()+random);
+    return element;
+};
+
+template<class T>void Bag<T>::clear(){
+    v.clear();
+};
+
+template<class T>unsigned int Bag<T>::size() const{
+    return v.size();
+};
+
+template<class T>bool Bag<T>::empty() {
+    return v.empty();
+};
+
+template<class T>const Bag<T>& Bag<T>::operator= (const Bag<T> & other){
+    v = other.v;
+};
+
 
 #endif
