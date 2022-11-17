7b3a972e88cfd86243add2745fa72fb7b278f9c4
TripiesRed <gonzalitoo2spam@gmail.com>
2022-11-17 20:57:04 +0100

    new

--- estudiante/include/dictionary.h
+++ estudiante/include/dictionary.h
@@ -1,10 +1,65 @@
+/**
+ * @file dictionary.h
+ * @brief  Archivo de especificación del TDA LettersSet
+ * @author Gonzalo Martínez Piédrola & Diego Ortega Sánchez
+ */
+
 #ifndef __DICTIONARY_H__
 #define __DICTIONARY_H__
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
 
 /**
  * @brief TDA Dictionary
  * @details Almacena las palabras de un fichero de texto y permite iterar sobre ellas
  *
  */
+ class Dictionary {
+
+ private:
+     set <string> words; //Conjunto de palabras del juego
+
+ public:
+
+     Dictionary()=default;
+
+     Dictionary(const Dictionary & other);
+
+     bool existe (const string & val) const;
+
+     bool insert (const string & val);
+
+     bool erase (const string & val);
+
+     void clear();
+
+     bool empty () const;
+
+     unsigned int size () const;
+
+     int getOccurrences (const char c);
+
+     int getTotalLetters ();
+
+     vector<string> wordsOfLength (int length);
+
+     friend ostream & operator<<(ostream &os, const Dictionary &dic);
+
+     friend istream & operator>>(istream &is, Dictionary &dic);
+
+     class iterator {
+
+     private:
+         set<string>::const_iterator it; //Iterador constante
+
+     public:
+
+     };
+
+ };
 
 #endif
--- estudiante/src/dictionary.cpp
+++ estudiante/src/dictionary.cpp
@@ -1,1 +1,76 @@
+/**
+ * @file dictionary.cpp
+ * @brief  Archivo de especificación del TDA LettersSet
+ * @author Gonzalo Martínez Piédrola & Diego Ortega Sánchez
+ */
+
 #include "dictionary.h"
+#include <iostream>
+
+using namespace std;
+
+/****************************************
+        FUNCIONES PÚBLICAS
+*****************************************/
+
+//Constructor de copia
+Dictionary::Dictionary(const Dictionary &other) {
+    words = other.words;
+}
+
+//Comprueba si el elemento ya está en el conjunto
+bool Dictionary::existe (const string & val) const{
+    set<string>::iterator it = words.begin();
+
+    for (int i = 0; i < val.length(); ++i)
+        while(it->at(i) != val.at(i)) it++;
+}
+
+//Añade un nuevo elemento al contenedor
+bool Dictionary::insert(const string &val) {
+    bool correcto = true;
+
+    if (empty() || !existe(val))
+        words.insert(val);
+
+    else correcto = false;
+
+    return correcto;
+}
+
+//Elimina el elemento del contenedor
+bool Dictionary::erase(const string &val) {
+    return words.erase(val);
+}
+
+//Borra todos los elementos del contenedor
+void Dictionary::clear() {
+    words.clear();
+}
+
+//Indica si el contenedor está vacío
+bool Dictionary::empty() const {
+    return words.empty();
+}
+
+//Devuelve el tamaño del contenedor
+unsigned int Dictionary::size() const {
+    return words.size();
+}
+
+//Calcula el valor total de una palabra
+int Dictionary::getOccurrences(const char c){
+    int total=0;
+    return total;
+}
+
+int Dictionary::getTotalLetters(){
+
+}
+
+vector<string> Dictionary::wordsOfLength (int length){
+
+}
+
+
+
